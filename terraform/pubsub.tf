fee188393c80077183f4eb4e172c4c8884b23af4
erlendev <erlend.vige@gmail.com>
2025-09-01 13:35:32 +0200

    Removes unnecessary iam in pubsub tf

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -11,14 +11,6 @@ resource "google_pubsub_subscription" "FilterNetexFileQueue" {
   }
 }
 
-resource "google_pubsub_subscription_iam_member" "ashur_filter_subscription_iam" {
-  project      = var.ashur_project
-  member       = var.service_account
-  role         = var.service_account_pubsub_subscriber_role
-  subscription = google_pubsub_subscription.FilterNetexFileQueue.name
-  depends_on   = [google_pubsub_subscription.FilterNetexFileQueue]
-}
-
 resource "google_pubsub_subscription" "FilterNetexFileStatusQueue" {
   name                       = "FilterNetexFileStatusQueue"
   topic                      = "projects/${var.pubsub_topic_project}/topics/FilterNetexFileStatusQueue"
@@ -30,12 +22,4 @@ resource "google_pubsub_subscription" "FilterNetexFileStatusQueue" {
     minimum_backoff = "10s"
     maximum_backoff = "600s"
   }
-}
-
-resource "google_pubsub_subscription_iam_member" "ashur_filter_status_subscription_iam" {
-  project      = var.ashur_project
-  member       = var.service_account
-  role         = var.service_account_pubsub_publisher_role
-  subscription = google_pubsub_subscription.FilterNetexFileStatusQueue.name
-  depends_on   = [google_pubsub_subscription.FilterNetexFileStatusQueue]
 }
