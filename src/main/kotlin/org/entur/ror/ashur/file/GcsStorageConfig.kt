26da0f38680d3c9c29695f47c75f2d3f4c33e297
erlendev <erlend.vige@gmail.com>
2025-09-12 08:24:59 +0200

    Fixes bad profile in GcsStorageConfig

--- src/main/kotlin/org/entur/ror/ashur/file/GcsStorageConfig.kt
+++ src/main/kotlin/org/entur/ror/ashur/file/GcsStorageConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.context.annotation.Scope
 
 @Configuration
-@Profile("gcs")
+@Profile("gcp")
 open class GcsStorageConfig {
     @Bean("mardukBlobStoreRepository")
     @Scope("prototype")
