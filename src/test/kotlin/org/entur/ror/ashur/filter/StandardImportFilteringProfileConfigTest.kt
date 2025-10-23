891e0fedf86e7dbcbe920419dc76242546e46045
erlendev <erlend.vige@gmail.com>
2025-10-23 16:02:29 +0200

    Adjusts filter config and bumps netex-tools

--- src/test/kotlin/org/entur/ror/ashur/filter/StandardImportFilteringProfileConfigTest.kt
+++ src/test/kotlin/org/entur/ror/ashur/filter/StandardImportFilteringProfileConfigTest.kt
@@ -13,16 +13,24 @@ class StandardImportFilteringProfileConfigTest {
         assertTrue(config.period.end!!.isEqual(LocalDate.now().plusYears(1)))
         assertTrue(config.skipElements.containsAll(
             listOf(
-                "VehicleScheduleFrame",
-                "DeadRun",
-                "TrainComponent",
-                "TrainElement",
-                "TrainNumber",
-                "Train",
-                "TrainInCompoundTrain",
-                "CompoundTrain",
-                "JourneyPart",
-                "serviceFacilitySets"
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/VehicleScheduleFrame",
+                "/PublicationDelivery/dataObjects/VehicleScheduleFrame",
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/ServiceFrame/lines/Line/routes",
+                "/PublicationDelivery/dataObjects/ServiceFrame/lines/Line/routes",
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/TimetableFrame/vehicleJourneys/DeadRun",
+                "/PublicationDelivery/dataObjects/TimetableFrame/vehicleJourneys/DeadRun",
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/TimetableFrame/trainNumbers",
+                "/PublicationDelivery/dataObjects/TimetableFrame/trainNumbers",
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/TimetableFrame/serviceFacilitySets",
+                "/PublicationDelivery/dataObjects/TimetableFrame/serviceFacilitySets",
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/ResourceFrame/dataSources",
+                "/PublicationDelivery/dataObjects/ResourceFrame/dataSources",
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/ResourceFrame/vehicleTypes",
+                "/PublicationDelivery/dataObjects/ResourceFrame/vehicleTypes",
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/TimetableFrame/vehicleJourneys/ServiceJourney/parts",
+                "/PublicationDelivery/dataObjects/TimetableFrame/vehicleJourneys/ServiceJourney/parts",
+                "/PublicationDelivery/dataObjects/CompositeFrame/frames/ResourceFrame/vehicles",
+                "/PublicationDelivery/dataObjects/ResourceFrame/vehicles",
             )
         ))
         assertTrue(config.removePrivateData)
