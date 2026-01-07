c094c0840568963943fe096f7808d4d5a757d126
erlendev <erlend.vige@gmail.com>
2026-01-07 11:53:15 +0100

    Adds missing terraform variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,6 +18,10 @@ variable "ashur_exchange_storage_bucket" {
   description = "The name of the Ashur exchange bucket in GCP"
 }
 
+variable "marduk_service_account" {
+  description = "The service account of the marduk application"
+}
+
 variable "marduk_service_account_exchange_bucket_role" {
   description = "Role of the Service Account - more about roles https://cloud.google.com/storage/docs/access-control/iam-roles"
   default     = "roles/storage.objectViewer"
