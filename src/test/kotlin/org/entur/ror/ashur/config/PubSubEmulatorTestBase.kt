455fb90e46373ecf0d8426eee78fca68e9300e14
erlendev <erlend.vige@gmail.com>
2026-01-30 10:39:02 +0100

    Fixes inconsistent tests
    * Fixes issues with tests behaving differently between IntelliJ and mvn test
    * Updates pubsub testcontainer instance to be a singleton, to ensure the test container is not shutdown after running a single test that depends on it. This could result in subsequent tests getting Connection refused when trying to communicate with pubsub (depending on how tests are run)

--- src/test/kotlin/org/entur/ror/ashur/config/PubSubEmulatorTestBase.kt
+++ src/test/kotlin/org/entur/ror/ashur/config/PubSubEmulatorTestBase.kt
@@ -4,27 +4,25 @@ import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PubSubEmulatorContainer
 import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
+@Testcontainers
 abstract class PubSubEmulatorTestBase {
+
     companion object {
-        @Container
-        val pubsubEmulator = PubSubEmulatorContainer(
-            DockerImageName.parse("gcr.io/google.com/cloudsdktool/cloud-sdk:emulators")
-        )
 
         @JvmStatic
         @DynamicPropertySource
         fun registerPubSubProperties(registry: DynamicPropertyRegistry) {
-            pubsubEmulator.start()
-            registry.add(
-                "spring.cloud.gcp.pubsub.emulator-host",
-                { pubsubEmulator.emulatorEndpoint }
-            )
-            registry.add(
-                "camel.component.google-pubsub.endpoint",
-                { pubsubEmulator.emulatorEndpoint }
-            )
+            val container = PubSubEmulatorSingleton.container
+
+            registry.add("spring.cloud.gcp.pubsub.emulator-host") {
+                container.emulatorEndpoint
+            }
+            registry.add("camel.component.google-pubsub.endpoint") {
+                container.emulatorEndpoint
+            }
         }
     }
 }
