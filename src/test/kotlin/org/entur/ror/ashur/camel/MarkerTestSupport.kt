578a36abeb4ec3420a8059b22e2493cac49247b4
erlendev <erlend.vige@gmail.com>
2026-07-27 15:34:02 +0200

    Fixes build

--- src/test/kotlin/org/entur/ror/ashur/camel/MarkerTestSupport.kt
+++ src/test/kotlin/org/entur/ror/ashur/camel/MarkerTestSupport.kt
@@ -1,6 +1,6 @@
 package org.entur.ror.ashur.camel
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import tools.jackson.module.kotlin.jacksonObjectMapper
 import net.logstash.logback.marker.LogstashMarker
 import org.slf4j.Marker
 import java.io.StringWriter
@@ -9,7 +9,7 @@ private val mapper = jacksonObjectMapper()
 
 internal fun Marker.fieldMap(): Map<String, Any?> {
     val out = StringWriter()
-    mapper.factory.createGenerator(out).use { gen ->
+    mapper.createGenerator(out).use { gen ->
         gen.writeStartObject()
         (this as LogstashMarker).writeTo(gen)
         gen.writeEndObject()
