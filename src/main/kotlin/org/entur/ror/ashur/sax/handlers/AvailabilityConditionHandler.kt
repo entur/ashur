734e894ba59a88b4e0ae2408fad9d354c25d6184
erlendev <erlend.vige@gmail.com>
2026-09-20 10:01:47 +0200

    Implement v0 of AvailabilityConditionHandler. Test is running green.

--- src/main/kotlin/org/entur/ror/ashur/sax/handlers/AvailabilityConditionHandler.kt
+++ src/main/kotlin/org/entur/ror/ashur/sax/handlers/AvailabilityConditionHandler.kt
@@ -1,8 +1,14 @@
 package org.entur.ror.ashur.sax.handlers
 
+import org.entur.netex.tools.lib.extensions.addNewAttribute
+import org.entur.netex.tools.lib.model.NetexTypes
+import org.entur.netex.tools.lib.output.NetexIdGenerator
 import org.entur.netex.tools.lib.output.XMLElementHandler
+import org.xml.sax.helpers.AttributesImpl
 
-class AvailabilityConditionHandler(private val codespace: String) : XMLElementHandler {
+class AvailabilityConditionHandler(
+    private val codespace: String
+) : XMLElementHandler {
     override fun startElement(
         uri: String?,
         localName: String?,
@@ -10,8 +16,17 @@ class AvailabilityConditionHandler(private val codespace: String) : XMLElementHa
         attributes: org.xml.sax.Attributes?,
         writer: org.entur.netex.tools.lib.output.DelegatingXMLElementWriter
     ) {
+        val id = NetexIdGenerator.next(codespace.uppercase(), NetexTypes.AVAILABILITY_CONDITION)
+        val newAttributes = AttributesImpl()
+        newAttributes.addNewAttribute("id", id)
+        writer.startElement(uri, localName, qName, newAttributes)
 
-        writer.startElement(uri, localName, qName, attributes)
+        writer.startElement("", "FromDate", "FromDate", null)
+        writer.characters("2026-01-01".toCharArray(), 0, "2026-01-01".length)
+        writer.endElement("", "FromDate", "FromDate")
+        writer.startElement("", "ToDate", "ToDate", null)
+        writer.characters("2026-01-02".toCharArray(), 0, "2026-01-02".length)
+        writer.endElement("", "ToDate", "ToDate")
     }
 
     override fun characters(
--- src/test/kotlin/org/entur/ror/ashur/sax/handlers/AvailabilityConditionHandlerTest.kt
+++ src/test/kotlin/org/entur/ror/ashur/sax/handlers/AvailabilityConditionHandlerTest.kt
@@ -5,8 +5,13 @@ import org.entur.netex.tools.lib.output.DelegatingXMLElementWriter
 import org.junit.Test
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.check
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.reset
 import org.mockito.kotlin.verify
+import org.xml.sax.helpers.AttributesImpl
+import kotlin.test.assertEquals
 
 class AvailabilityConditionHandlerTest {
     val codespace = "tst"
@@ -34,11 +39,23 @@ class AvailabilityConditionHandlerTest {
             writer
         )
 
-        verify(writer).startElement("", NetexTypes.AVAILABILITY_CONDITION, NetexTypes.AVAILABILITY_CONDITION, null)
+        verify(writer).startElement(
+            eq(""),
+            eq(NetexTypes.AVAILABILITY_CONDITION),
+            eq(NetexTypes.AVAILABILITY_CONDITION),
+            check {
+                assertEquals("TST:AvailabilityCondition:1", it.getValue("id"))
+            }
+        )
+
         verify(writer).startElement("", NetexTypes.FROM_DATE, NetexTypes.FROM_DATE, null)
+
         verify(writer).endElement("", NetexTypes.FROM_DATE, NetexTypes.FROM_DATE)
+
         verify(writer).startElement("", NetexTypes.TO_DATE, NetexTypes.TO_DATE, null)
+
         verify(writer).endElement("", NetexTypes.TO_DATE, NetexTypes.TO_DATE)
+
         verify(writer).endElement("", NetexTypes.AVAILABILITY_CONDITION, NetexTypes.AVAILABILITY_CONDITION)
     }
 
